a54bd61f44cffb11965894b6223c156584a04ac8
Alejandrodelosrios <alejandrodelosrios09@gmail.com>
2025-05-23 16:15:49 -0400

    progreso de lo de inmuebles

--- src/modules/state/models/state.model.ts
+++ src/modules/state/models/state.model.ts
@@ -4,24 +4,38 @@ import { type User } from "@/modules/users/models/user.model";
 import { type Ubicacion } from "./ubicacion.model";
 import { type CreateUbicacion } from "./ubicacion.model";
 import { type Sector } from "@/models/sector.model";
+import { Category } from "@/models/category.model";
+import { Modality } from "@/models/modality.model";
+import { Imagen } from "./imagen.models";
+import { Owner } from "@/models/owner.model";
+
+export interface PropertyOwner extends ApiBase{
+  owner: Owner;
+}
+
 
 export interface State extends ApiBase{
  descripcion:string
  precio: number
  estado: ESTADO
  area: number
- NroHabitaciones?: number
- NroBanos?: number
- NroEstacionamientos?:number
+ NroHabitaciones: number
+ NroBanos: number
+ NroEstacionamientos:number
+ comision:number //<- recien se agrego
+ condicion_Compra: string //<- recien se agrego
+
  user:User
- category:string// Category reemplazar aqui cuando suban category 
- modality: string //Modality reemplazar aqui cuando suban modality
+ category:Category 
+ modality:Modality 
  sector: Sector 
  ubicacion: Ubicacion
-}
+ imagenes: Imagen[]  //<- segun chatgpt esto debe traer las imagenes del inmueble 
+ property_owner: PropertyOwner[]; // <- segun chat gpt esto debe traer los dueños del inmueble
+ }
 
 export interface CreateState extends Partial<Omit<State,'user'|'category'|'modality'|'sector'|'ubicacion'|'estado'>>{
- estado: string 
+ estado: ESTADO
  user: string
  category:string
  modality:string
