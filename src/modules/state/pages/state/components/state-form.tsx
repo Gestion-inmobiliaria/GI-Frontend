b6266d775018924aa034c6b7cfdd01f6e6521326
Alejandrodelosrios <alejandrodelosrios09@gmail.com>
2025-05-29 00:03:53 -0400

    formulario actualizado

--- src/modules/state/pages/state/components/state-form.tsx
+++ src/modules/state/pages/state/components/state-form.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   Form,
-  FormControl,
+  FormControl, 
   FormField,
   FormItem,
   FormLabel,
